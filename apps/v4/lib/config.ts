0dddab68ace9f1845483714dcff468821f9acea3
panos <varoufakiskomouni@gmail.com>
2025-11-07 06:09:08 +0200

    rebrand finished

--- apps/v4/lib/config.ts
+++ apps/v4/lib/config.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
   description:
     "A set of beautifully designed components that you can customize, extend, and build on. Start here then make it your own. Open Source. Open Code.",
   links: {
-    twitter: "https://twitter.com/pitsi",
+    twitter: "https://x.com/panos_dev_",
     github: "https://github.com/PanagiotisPitsikoulis/pitsi-ui",
   },
   navItems: [
