353bcbfbaf3407cc8fc3045865fcdf383abe10f3
panos <varoufakiskomouni@gmail.com>
2025-12-09 07:09:44 +0200

    fix

--- apps/v4/lib/config/index.ts
+++ apps/v4/lib/config/index.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "pitsi/ui",
-  url: "https://pitsiui.com",
+  url: process.env.NEXT_PUBLIC_APP_URL || "https://pitsiui.com",
   ogImage: "https://pitsiui.com/og.jpg",
   description:
     "A set of beautifully designed components that you can customize, extend, and build on. Start here then make it your own. Open Source. Open Code.",
